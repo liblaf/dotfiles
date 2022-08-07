9b22deb02223cdfb4d3b42838bc0d24607f65272
Qin Li <liblaf@outlook.com>
2022-08-08 03:27:59 +0800

    feat(ubuntu): add color plugin

---
+++ ubuntu/.oh-my-zsh/custom/plugins/color/color.plugin.zsh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+function color-table() {
+  local csi="\x1b["
+  local reset="${csi}0m"
+  for mode in {0..9}; do
+    for foreground in {30..37} 39; do
+      for background in {40..47} 49; do
+        echo -n "${csi}${mode}m${csi}${foreground}m${csi}${background}m${mode};${foreground};${background}\t${reset}"
+      done
+      echo
+    done
+    echo
+  done
+}
+
+function color-makefile() {
+  cat <<-EOF | tee --append Makefile >"/dev/null"
+ERROR   := \$(shell tput bold)\$(shell tput setaf 1)
+SUCCESS := \$(shell tput bold)\$(shell tput setaf 2)
+WARNING := \$(shell tput bold)\$(shell tput setaf 3)
+INFO    := \$(shell tput bold)\$(shell tput setaf 4)
+RESET   := \$(shell tput init)
+EOF
+}
