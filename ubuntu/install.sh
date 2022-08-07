21f8effab819fd73755ea2545a887b50b58c6313
Qin Li <liblaf@outlook.com>
2022-08-08 03:31:46 +0800

    build(ubuntu): add install.sh

---
+++ ubuntu/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -o xtrace
+set -o errexit
+set -o nounset
+
+cd "$(dirname "${0}")"
+prefix="$(pwd)"
+git submodule update --init --depth 1 --recursive
+rm --force --recursive "${ZSH_CUSTOM:-"${ZSH:-"${HOME}/.oh-my-zsh"}/custom"}"
+cp --recursive "${prefix}/.oh-my-zsh/custom/" "${ZSH_CUSTOM:-"${ZSH:-"${HOME}/.oh-my-zsh"}/custom"}"
+cp "${prefix}/.zshrc" "${HOME}/"
+cp "${prefix}/.zprofile" "${HOME}/"
