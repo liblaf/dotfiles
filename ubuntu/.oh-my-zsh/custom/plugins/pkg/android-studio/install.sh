594618f3034f39c2f0938a63db1e18ec49cb1ba6
Qin Li <liblaf@outlook.com>
2022-08-08 04:04:59 +0800

    fix(ubuntu): mkdir before access

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/android-studio/install.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/android-studio/install.sh
@@ -7,16 +7,20 @@ version="${1:-"2021"}"
 case "${version}" in
 "2" | "2.3" | "2.3.3")
   filename="android-studio-ide-162.4069837-linux.zip"
+  mkdir --parents "${HOME}/Downloads/"
   filepath="${HOME}/Downloads/${filename}"
   wget --output-document="-" "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2.3.3.0/${filename}" |
     tee "${filepath}" >"/dev/null"
+  mkdir --parents "${HOME}/.local/pkgs/android-studio/2.3.3/"
   7zz x "${filepath}" -o"${HOME}/.local/pkgs/android-studio/2.3.3/"
   ;;
 "2.3.2")
   filename="android-studio-ide-162.3934792-linux.zip"
+  mkdir --parents "${HOME}/Downloads/"
   filepath="${HOME}/Downloads/${filename}"
   wget --output-document="-" "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2.3.2.0/${filename}" |
     tee "${filepath}" >"/dev/null"
+  mkdir --parents "${HOME}/.local/pkgs/android-studio/2.3.2/"
   7zz x "${filepath}" -o"${HOME}/.local/pkgs/android-studio/2.3.2/"
   ;;
 *)
--- ubuntu/.oh-my-zsh/custom/plugins/pkg/ndk/install.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/ndk/install.sh
@@ -7,21 +7,25 @@ version="${1:-"r25"}"
 case "${version}" in
 "r21" | "r21e" | "21" | "21.4" | "21.4.7075529")
   filename="android-ndk-r21e-linux-x86_64.zip"
+  mkdir --parents "${HOME}/Downloads/"
   filepath="${HOME}/Downloads/${filename}"
   wget --output-document="-" "https://dl.google.com/android/repository/${filename}" |
     tee "${filepath}" >"/dev/null"
+  mkdir --parents "${HOME}/.local/pkgs/ndk/"
   7zz x "${filepath}" -o"${HOME}/.local/pkgs/ndk/"
-  rm --force --recursive "${HOME}/.local/pkgs/ndk/r21e/"
-  mv "${HOME}/.local/pkgs/ndk/android-ndk-r21e/" "${HOME}/.local/pkgs/ndk/r21e/"
+  rm --force --recursive "${HOME}/.local/pkgs/ndk/r21e"
+  mv "${HOME}/.local/pkgs/ndk/android-ndk-r21e" "${HOME}/.local/pkgs/ndk/r21e"
   ;;
 "r14" | "r14b")
   filename="android-ndk-r14b-linux-x86_64.zip"
+  mkdir --parents "${HOME}/Downloads/"
   filepath="${HOME}/Downloads/${filename}"
   wget --output-document="-" "https://dl.google.com/android/repository/${filename}" |
     tee "${filepath}" >"/dev/null"
+  mkdir --parents "${HOME}/.local/pkgs/ndk/"
   7zz x "${filepath}" -o"${HOME}/.local/pkgs/ndk/"
-  rm --force --recursive "${HOME}/.local/pkgs/ndk/r14b/"
-  mv "${HOME}/.local/pkgs/ndk/android-ndk-r14b/" "${HOME}/.local/pkgs/ndk/r14b/"
+  rm --force --recursive "${HOME}/.local/pkgs/ndk/r14b"
+  mv "${HOME}/.local/pkgs/ndk/android-ndk-r14b" "${HOME}/.local/pkgs/ndk/r14b"
   ;;
 *)
   echo "This script does not support NDK version \"${version}\""
