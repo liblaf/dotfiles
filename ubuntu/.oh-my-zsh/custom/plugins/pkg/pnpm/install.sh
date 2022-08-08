a4c0c19f5649599fa340fbb89c9c121dd0888972
Qin Li <liblaf@outlook.com>
2022-08-09 02:53:35 +0800

    feat(pkg): install additional packages along with `pnpm`

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/pnpm/install.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/pnpm/install.sh
@@ -6,3 +6,12 @@ brew install node
 brew install pnpm
 
 pnpm setup
+pnpm install-completion zsh
+pnpm add --global commitizen
+pnpm add --global cz-conventional-changelog
+pnpm add --global npm-check-updates
+cat <<-EOF | tee "${HOME}/.czrc" >"/dev/null"
+{
+  "path": "cz-conventional-changelog"
+}
+EOF
