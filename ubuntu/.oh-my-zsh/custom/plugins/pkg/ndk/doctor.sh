26f816d36b73b31c22ad23c44d20e8962a5a4124
Qin Li <liblaf@outlook.com>
2022-08-13 14:35:29 +0800

    feat(pkg): add `ndk/doctor.sh`

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/ndk/doctor.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/ndk/doctor.sh
@@ -2,16 +2,22 @@
 set -o errexit
 set -o nounset
 
+source "$(dirname "$(dirname "${0}")")/utils.sh"
+
 version="${1:-"r25"}"
 
 case "${version}" in
+"r25" | "25.0.8775105")
+  filename="clang-r450784d.tar.gz"
+  filepath="${HOME}/Downloads/${filename}"
+  download "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/ndk-r25/${filename}" "${filepath}"
+  extract "${filepath}" "${HOME}/.local/pkgs/ndk/r25/toolchains/llvm/prebuilt/linux-x86_64/"
+  ;;
 "r21" | "r21e" | "21" | "21.4" | "21.4.7075529")
   filename="clang-r365631c3.tar.gz"
   filepath="${HOME}/Downloads/${filename}"
-  wget --output-document="-" "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/ndk-r21e/${filename}" |
-    tee "${filepath}" >"/dev/null"
-  pv "${filepath}" |
-    tar --extract --skip-old-files --gzip --directory="${HOME}/.local/pkgs/ndk/r21e/toolchains/llvm/prebuilt/linux-x86_64/"
+  download "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/ndk-r21e/${filename}" "${filepath}"
+  extract "${filepath}" "${HOME}/.local/pkgs/ndk/r21e/toolchains/llvm/prebuilt/linux-x86_64/"
   ;;
 *)
   echo "This script does not support NDK version \"${version}\""
