c3e3f409a63d8cdfdc84878b55ba807886e0170c
Qin Li <liblaf@outlook.com>
2022-09-15 10:37:59 +0800

    fix(ialias): display icons next to file names

--- ubuntu/.oh-my-zsh/custom/plugins/ialias/ialias.plugin.zsh
+++ ubuntu/.oh-my-zsh/custom/plugins/ialias/ialias.plugin.zsh
@@ -2,12 +2,12 @@
 
 # exa
 if command -v exa >"/dev/null"; then
-  alias l="exa --long --all --header --git"
-  alias la="exa --long --all --header --git"
-  alias ll="exa --long --header --git"
+  alias l="exa --long --icons --all --header --git"
+  alias la="exa --long --icons --all --header --git"
+  alias ll="exa --long --icons --header --git"
   alias ls="exa --git"
-  alias lsa="exa --long --all --all --header --git"
+  alias lsa="exa --long --icons --all --all --header --git"
   if ! command -v tree >"/dev/null"; then
-    alias tree="exa --tree"
+    alias tree="exa --tree --icons"
   fi
 fi
