7715b85ae5b1ff6084cd5706a49c2633e0d4b306
Qin Li <liblaf@outlook.com>
2022-09-20 18:53:09 +0800

    build(pkg): bump Clash for Windows

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/clash-for-windows/install.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/clash-for-windows/install.sh
@@ -4,9 +4,10 @@ set -o nounset
 
 source "$(dirname "$(dirname "${0}")")/utils.sh"
 
-filename="Clash.for.Windows-0.19.26-x64-linux.tar.gz"
+version="${version:-"0.20.3"}"
+filename="Clash.for.Windows-${version}-x64-linux.tar.gz"
 mkdir --parents "${HOME}/Downloads/"
 filepath="${HOME}/Downloads/${filename}"
-download "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/0.19.26/${filename}" "${filepath}"
+download "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/${version}/${filename}" "${filepath}"
 extract "${filepath}" "${HOME}/.local/pkgs/"
-mv "${HOME}/.local/pkgs/Clash for Windows-0.19.26-x64-linux" "${HOME}/.local/pkgs/clash-for-windows"
+mv "${HOME}/.local/pkgs/Clash for Windows-${version}-x64-linux" "${HOME}/.local/pkgs/clash-for-windows"
