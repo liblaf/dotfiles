b3ba82f4e6e1e8e041f9b2463cd1d73fffb5f64c
Qin Li <liblaf@outlook.com>
2022-09-20 19:06:35 +0800

    refactor(pkg): improve code quality

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/android-studio/install.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/android-studio/install.sh
@@ -6,23 +6,41 @@ source "$(dirname "$(dirname "${0}")")/utils.sh"
 
 version="${1:-"2021"}"
 
-case "${version}" in
-"2" | "2.3" | "2.3.3")
-  filename="android-studio-ide-162.4069837-linux.zip"
+function install-android-studio() {
   filepath="${HOME}/Downloads/${filename}"
-  download "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2.3.3.0/${filename}" "${filepath}"
+  download "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/${version}/${filename}" "${filepath}"
   extract "${filepath}" "${HOME}/.local/pkgs/android-studio/"
-  rm --force --recursive "${HOME}/.local/pkgs/android-studio/2.3.3/"
-  mv "${HOME}/.local/pkgs/android-studio/android-studio/" "${HOME}/.local/pkgs/android-studio/2.3.3/"
+  rm --force --recursive "${HOME}/.local/pkgs/android-studio/${version}/"
+  mv "${HOME}/.local/pkgs/android-studio/android-studio/" "${HOME}/.local/pkgs/android-studio/${version}/"
+  export DESKTOP_FILE_INSTALL_DIR="${HOME}/.local/share/applications"
+  mkdir --parents "${DESKTOP_FILE_INSTALL_DIR}"
+  local desktop_entry="${DESKTOP_FILE_INSTALL_DIR}/android-studio-${version}.desktop"
+  cat >"${desktop_entry}" <<-EOF
+[Desktop Entry]
+Type=Application
+Name=Android Studio ${version}
+Exec=${HOME}/.local/pkgs/android-studio/${version}/bin/studio.sh
+Icon=${HOME}/.local/pkgs/android-studio/${version}/bin/studio.png
+Terminal=false
+EOF
+  desktop-file-install --dir "${DESKTOP_FILE_INSTALL_DIR}" "${desktop_entry}"
+}
+
+case "${version}" in
+"2021" | "2021.3" | "2021.3.1" | "2021.3.1.16")
+  version="2021.3.1.16"
+  filename="android-studio-2021.3.1.16-linux.tar.gz"
+  install-android-studio
   ;;
-"2.3.2")
+"2" | "2.3" | "2.3.3" | "2.3.3.0")
+  version="2.3.3.0"
+  filename="android-studio-ide-162.4069837-linux.zip"
+  install-android-studio
+  ;;
+"2.3.2" | "2.3.2.0")
+  version="2.3.2.0"
   filename="android-studio-ide-162.3934792-linux.zip"
-  mkdir --parents "${HOME}/Downloads/"
-  filepath="${HOME}/Downloads/${filename}"
-  download "https://redirector.gvt1.com/edgedl/android/studio/ide-zips/2.3.2.0/${filename}" "${filepath}"
-  extract "${filepath}" -o"${HOME}/.local/pkgs/android-studio/"
-  rm --force --recursive "${HOME}/.local/pkgs/android-studio/2.3.2/"
-  mv "${HOME}/.local/pkgs/android-studio/android-studio/" "${HOME}/.local/pkgs/android-studio/2.3.2/"
+  install-android-studio
   ;;
 *)
   echo "This script does not support Android Studio version \"${version}\""
--- ubuntu/.oh-my-zsh/custom/plugins/pkg/android-studio/uninstall.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/android-studio/uninstall.sh
@@ -4,6 +4,10 @@ set -o nounset
 
 version="${1:-"all"}"
 
+function uninstall-android-studio() {
+  rm --force --recursive "${HOME}/.local/pkgs/android-studio/${version}/"
+}
+
 case "${version}" in
 "all")
   rm --force --recursive "${HOME}/.local/pkgs/android-studio/"
@@ -11,11 +15,17 @@ case "${version}" in
   rm --force --recursive "${HOME}/.AndroidStudio2.3/"
   rm --force --recursive "${HOME}/.android/"
   ;;
-"2" | "2.3" | "2.3.3")
-  rm --force --recursive "${HOME}/.local/pkgs/android-studio/2.3.3/"
+"2021" | "2021.3" | "2021.3.1" | "2021.3.1.16")
+  version="2021.3.1.16"
+  uninstall-android-studio
   ;;
-"2.3.2")
-  rm --force --recursive "${HOME}/.local/pkgs/android-studio/2.3.2/"
+"2" | "2.3" | "2.3.3" | "2.3.3.0")
+  version="2.3.3.0"
+  uninstall-android-studio
+  ;;
+"2.3.2" | "2.3.2.0")
+  version="2.3.2.0"
+  uninstall-android-studio
   ;;
 *)
   echo "This script does not support Android Studio version \"${version}\""
