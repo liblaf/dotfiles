91e221b599c7bde937acbafb8f88e081143280ce
Qin Li <liblaf@outlook.com>
2022-09-22 19:54:24 +0800

    feat(pkg): support `copy-prebuilt` for NDK management

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/ndk/doctor.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/ndk/doctor.sh
@@ -4,24 +4,72 @@ set -o nounset
 
 source "$(dirname "$(dirname "${0}")")/utils.sh"
 
-version="${1:-"r25"}"
+function get-version() {
+  case "${1}" in
+  "r25b" | "25" | "25.1" | "25.1.8937393")
+    ndk_version="25.1.8937393"
+    ndk_tag="r25b"
+    ;;
+  "r24" | "24" | "24.0" | "24.0.8215888")
+    ndk_version="24.0.8215888"
+    ndk_tag="r24"
+    ;;
+  "r23c" | "23" | "23.2" | "23.2.8568313")
+    ndk_version="23.2.8568313"
+    ndk_tag="r23c"
+    ;;
+  "r22b" | "22" | "22.1" | "22.1.7171670")
+    ndk_version="22.1.7171670"
+    ndk_tag="r22b"
+    ;;
+  "r21e" | "21" | "21.4" | "21.4.7075529")
+    ndk_version="21.4.7075529"
+    ndk_tag="r21e"
+    ;;
+  *)
+    echo "This script does not support NDK version \"${1}\""
+    ;;
+  esac
+  echo "NDK Version   : ${ndk_version}"
+  echo "NDK Tag       : ${ndk_tag}"
+  ANDROID_NDK="${HOME}/Android/Sdk/ndk/${ndk_version}"
+  LLVM_HOME="${ANDROID_NDK}/toolchains/llvm/prebuilt/linux-x86_64"
+}
 
-case "${version}" in
-"r25" | "25.0.8775105")
-  filename="clang-r450784d.tar.gz"
-  filepath="${HOME}/Downloads/${filename}"
-  download "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/ndk-r25/${filename}" "${filepath}"
-  extract "${filepath}" "${HOME}/.local/pkgs/ndk/r25/toolchains/llvm/prebuilt/linux-x86_64/"
+function install-prebuilt() {
+  get-version "${1}"
+  local clang_version="$(sed --quiet '2p' "${LLVM_HOME}/AndroidVersion.txt" | awk '{ print $3 }')"
+  echo "Clang Tag     : ${clang_version}"
+  local filename="clang-${clang_version}.tar.gz"
+  local filepath="${HOME}/Downloads/${filename}"
+  download "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/ndk-${ndk_tag}/${filename}" "${filepath}"
+  extract "${filepath}" "${LLVM_HOME}"
+}
+
+function copy-prebuilt() {
+  get-version "${1}"
+  local SOURCE_LLVM_HOME="${LLVM_HOME}"
+  get-version "${2}"
+  local DEST_LLVM_HOME="${LLVM_HOME}"
+  rm --force --recursive "${DEST_LLVM_HOME}"
+  cp --recursive "${SOURCE_LLVM_HOME}" "${DEST_LLVM_HOME}"
+}
+
+if [[ -n "${1}" ]]; then
+  sub_command="${1}"
+  shift 1
+else
+  sub_command="install-prebuilt"
+fi
+
+case "${sub_command}" in
+"install-prebuilt")
+  install-prebuilt "${@}"
   ;;
-"r21" | "r21e" | "21" | "21.4" | "21.4.7075529")
-  filename="clang-r365631c3.tar.gz"
-  filepath="${HOME}/Downloads/${filename}"
-  download "https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/ndk-r21e/${filename}" "${filepath}"
-  extract "${filepath}" "${HOME}/.local/pkgs/ndk/r21e/toolchains/llvm/prebuilt/linux-x86_64/"
-  rm "${HOME}/.local/pkgs/ndk/r21e/toolchains/llvm/prebuilt/linux-x86_64/bin/clang.real"
-  rm "${HOME}/.local/pkgs/ndk/r21e/toolchains/llvm/prebuilt/linux-x86_64/bin/clang++.real"
+"copy-prebuilt")
+  copy-prebuilt "${@}"
   ;;
 *)
-  echo "This script does not support NDK version \"${version}\""
+  echo "Unknown sub-command \"${sub_command}\""
   ;;
 esac
--- ubuntu/.oh-my-zsh/custom/plugins/pkg/ndk/install.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/ndk/install.sh
@@ -4,32 +4,31 @@ set -o nounset
 
 source "$(dirname "$(dirname "${0}")")/utils.sh"
 
-version="${1:-"r25"}"
+echo "Do not use this script to install NDK."
+echo "Use SDK Manager in Android Studio instead."
+exit 1
+
+version="${1}"
+
+function install-ndk() {
+  filepath="${HOME}/Downloads/${filename}"
+  ANDROID_NDK="${ANDROID_NDK:-"${HOME}/Android/Sdk/ndk/${version}"}"
+  download "https://dl.google.com/android/repository/${filename}" "${filepath}"
+  extract "${filepath}" "${HOME}/Android/Sdk/ndk/"
+  rm --force --recursive "${ANDROID_NDK}"
+  mv ${HOME}/Android/Sdk/ndk/android-ndk-* "${ANDROID_NDK}"
+}
 
 case "${version}" in
-"r25" | "25.0.8775105")
-  filename="android-ndk-r25-linux.zip"
-  filepath="${HOME}/Downloads/${filename}"
-  download "https://dl.google.com/android/repository/${filename}" "${filepath}"
-  extract "${filepath}" "${HOME}/.local/pkgs/ndk/"
-  rm --force --recursive "${HOME}/.local/pkgs/ndk/r25"
-  mv "${HOME}/.local/pkgs/ndk/android-ndk-r25" "${HOME}/.local/pkgs/ndk/r25"
+"r25b" | "25" | "25.1" | "25.1.8937393")
+  version="25.1.8937393"
+  filename="android-ndk-r25b-linux.zip"
+  install-ndk
   ;;
-"r21" | "r21e" | "21" | "21.4" | "21.4.7075529")
+"r21e" | "21" | "21.4" | "21.4.7075529")
+  version="21.4.7075529"
   filename="android-ndk-r21e-linux-x86_64.zip"
-  filepath="${HOME}/Downloads/${filename}"
-  download "https://dl.google.com/android/repository/${filename}" "${filepath}"
-  extract "${filepath}" "${HOME}/.local/pkgs/ndk/"
-  rm --force --recursive "${HOME}/.local/pkgs/ndk/r21e"
-  mv "${HOME}/.local/pkgs/ndk/android-ndk-r21e" "${HOME}/.local/pkgs/ndk/r21e"
-  ;;
-"r14" | "r14b")
-  filename="android-ndk-r14b-linux-x86_64.zip"
-  filepath="${HOME}/Downloads/${filename}"
-  download "https://dl.google.com/android/repository/${filename}" "${filepath}"
-  extract "${filepath}" "${HOME}/.local/pkgs/ndk/"
-  rm --force --recursive "${HOME}/.local/pkgs/ndk/r14b"
-  mv "${HOME}/.local/pkgs/ndk/android-ndk-r14b" "${HOME}/.local/pkgs/ndk/r14b"
+  install-ndk
   ;;
 *)
   echo "This script does not support NDK version \"${version}\""
