04a24c3976e7f6e512a5f545ce24f743aad7221c
Qin Li <liblaf@outlook.com>
2022-09-22 19:31:44 +0800

    fix(pkg): add desktop entry of `Clash for Windows`

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/clash-for-windows/install.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/clash-for-windows/install.sh
@@ -10,4 +10,16 @@ mkdir --parents "${HOME}/Downloads/"
 filepath="${HOME}/Downloads/${filename}"
 download "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/${version}/${filename}" "${filepath}"
 extract "${filepath}" "${HOME}/.local/pkgs/"
+rm --force --recursive "${HOME}/.local/pkgs/clash-for-windows"
 mv "${HOME}/.local/pkgs/Clash for Windows-${version}-x64-linux" "${HOME}/.local/pkgs/clash-for-windows"
+download "https://github.com/Dreamacro/clash/raw/master/docs/logo.png" "${HOME}/.local/pkgs/clash-for-windows/logo.png"
+DESKTOP_FILE_INSTALL_DIR="${HOME}/.local/share/applications"
+cat >"${DESKTOP_FILE_INSTALL_DIR}/clash-for-windows.desktop" <<-EOF
+[Desktop Entry]
+Type=Application
+Name=Clash for Windows
+Exec=${HOME}/.local/pkgs/clash-for-windows/cfw
+Icon=${HOME}/.local/pkgs/clash-for-windows/logo.png
+Terminal=false
+EOF
+desktop-file-install --dir="${DESKTOP_FILE_INSTALL_DIR}" "${DESKTOP_FILE_INSTALL_DIR}/clash-for-windows.desktop"
