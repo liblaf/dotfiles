2a9d15ca18e99d95248f6803cd160d2d27570516
Qin Li <liblaf@outlook.com>
2022-09-24 19:34:57 +0800

    style(pkg): reformat

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/conda/install.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/conda/install.sh
@@ -3,13 +3,12 @@
 set -o errexit
 set -o nounset
 
-source "$(dirname "$(dirname "${0}")")/utils.sh"
+source "$(dirname "$(dirname "$(realpath "${0}")")")/utils.sh"
 
 filename="Miniconda3-latest-Linux-x86_64.sh"
 filepath="${HOME}/Downloads/${filename}"
-
 download "https://repo.anaconda.com/miniconda/${filename}" "${filepath}"
+
 mkdir --parents "${HOME}/.local/pkgs/"
 bash "${filepath}" -b -p "${HOME}/.local/pkgs/conda"
-
 "${HOME}/.local/pkgs/conda/bin/conda" init zsh
