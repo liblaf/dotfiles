10ad214e756598a83642e3243f0eb67f1e1c3531
Qin Li <liblaf@outlook.com>
2022-09-24 19:33:36 +0800

    fix(pkg): fix `clash-for-windows`

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/clash-for-windows/install.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/clash-for-windows/install.sh
@@ -6,12 +6,14 @@ source "$(dirname "$(dirname "${0}")")/utils.sh"
 
 version="${version:-"0.20.3"}"
 filename="Clash.for.Windows-${version}-x64-linux.tar.gz"
-mkdir --parents "${HOME}/Downloads/"
 filepath="${HOME}/Downloads/${filename}"
 download "https://github.com/Fndroid/clash_for_windows_pkg/releases/download/${version}/${filename}" "${filepath}"
+
 extract "${filepath}" "${HOME}/.local/pkgs/"
 rm --force --recursive "${HOME}/.local/pkgs/clash-for-windows"
 mv "${HOME}/.local/pkgs/Clash for Windows-${version}-x64-linux" "${HOME}/.local/pkgs/clash-for-windows"
+
+# install desktop entry
 download "https://github.com/Dreamacro/clash/raw/master/docs/logo.png" "${HOME}/.local/pkgs/clash-for-windows/logo.png"
 DESKTOP_FILE_INSTALL_DIR="${HOME}/.local/share/applications"
 cat >"${DESKTOP_FILE_INSTALL_DIR}/clash-for-windows.desktop" <<-EOF
--- ubuntu/.oh-my-zsh/custom/plugins/pkg/clash-for-windows/uninstall.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/clash-for-windows/uninstall.sh
@@ -3,3 +3,6 @@ set -o errexit
 set -o nounset
 
 rm --force --recursive "${HOME}/.local/pkgs/clash-for-windows/"
+
+DESKTOP_FILE_INSTALL_DIR="${HOME}/.local/share/applications"
+rm --force "${DESKTOP_FILE_INSTALL_DIR}/clash-for-windows.desktop"
