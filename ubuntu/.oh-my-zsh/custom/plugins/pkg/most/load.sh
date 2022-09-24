aeadc92f1e45c11ae4e861a1478b4b150404b486
Qin Li <liblaf@outlook.com>
2022-09-24 19:36:16 +0800

    revert(pkg): remove `most` pkg

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/most/load.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [[ -x "$(command -v most)" ]]; then
-  export PAGER="$(command -v most)"
-else
-  echo "\"most\" not found"
-fi
--- ubuntu/.oh-my-zsh/custom/plugins/pkg/most/uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-set -o nounset
-
-brew uninstall most
