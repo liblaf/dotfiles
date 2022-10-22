a276cf520fb61186f9ce440b52ad48159f638cfb
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-10-22 15:09:20 +0800

    fix(ohmyzsh): add Google Fonts

--- ubuntu/.oh-my-zsh/custom/plugins/pkg/fonts/install.sh
+++ ubuntu/.oh-my-zsh/custom/plugins/pkg/fonts/install.sh
@@ -1,15 +1,21 @@
-#!/usr/bin/env bash
-# https://github.com/romkatv/powerlevel10k#fonts
+#!/usr/bin/bash
 set -o errexit
 set -o nounset
 
 source "$(dirname "$(dirname "${0}")")/utils.sh"
 
+# https://github.com/romkatv/powerlevel10k#fonts
 for style in "Regular" "Bold" "Italic" "Bold Italic"; do
   filename="MesloLGS NF ${style}.ttf"
-  echo "${filename}"
-  filepath="${HOME}/.local/share/fonts/${filename}"
+  filepath="${HOME}/.local/share/fonts/MesloLGS NF/${filename}"
   url="https://github.com/romkatv/powerlevel10k-media/raw/master/${filename}"
   download "${url}" "${filepath}"
 done
+
+filename="08_NotoSansCJKsc.zip"
+filepath="${HOME}/Downloads/${filename}"
+url="https://mirrors.tuna.tsinghua.edu.cn/github-release/googlefonts/noto-cjk/LatestRelease/${filename}"
+download "${url}" "${filepath}"
+extract "${filepath}" "${HOME}/.local/share/fonts/08_NotoSansCJKsc/"
+
 fc-cache --force --verbose
