f7bb06c5328ca16f062ce3714c61dcd32e4def68
Qin Li <30631553+liblaf@users.noreply.github.com>
2022-11-23 22:44:04 +0800

    refactor(apt): pretty output

--- ubuntu/make/apt.mk
+++ ubuntu/make/apt.mk
@@ -7,14 +7,13 @@ APT_PKGS += libfuse2 libffi7
 apt: apt-mirror $(addsuffix -apt, $(APT_PKGS))
 
 .PHONY: apt-mirror
-apt-mirror:
-	# https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/
-	@ call sudo sed -i "s@http://.*archive.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
-	@ call sudo sed -i "s@http://.*security.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+apt-mirror: scripts/apt.sh
+	@ # https://mirrors.tuna.tsinghua.edu.cn/help/ubuntu/
+	@ bash scripts/apt.sh < /dev/null
 
 define apt-rule =
 .PHONY: $(1)-apt
 $(1)-apt:
-	@ if ! apt list $(1) 2>&1 | grep "\[installed\]" > /dev/null 2>&1; then call sudo apt install $(1);	fi
+	@ if ! apt list $(1) 2>&1 | grep "installed" > /dev/null 2>&1; then call sudo apt install $(1);	fi
 endef
 $(foreach pkg, $(APT_PKGS), $(eval $(call apt-rule, $(pkg))))
---
+++ ubuntu/scripts/apt.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source scripts/init.sh
+
+backup=/etc/apt/sources.list.save
+if [[ ! -e ${backup} ]]; then
+  sudo cp /etc/apt/sources.list "${backup}"
+  success "Copy: /etc/apt/sources.list -> ${backup}"
+fi
+
+if grep --color "http://.*archive.ubuntu.com" /etc/apt/sources.list; then
+  call sudo sed -i "s@http://.*archive.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+fi
+if grep --color "http://.*security.ubuntu.com" /etc/apt/sources.list; then
+  call sudo sed -i "s@http://.*security.ubuntu.com@https://mirrors.tuna.tsinghua.edu.cn@g" /etc/apt/sources.list
+fi
