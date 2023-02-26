7ea055394898756957347aa2346113da2406c18c
Qin Li <liblaf@outlook.com>
2023-02-26 20:36:41 +0800

    feat: add upgit

--- ubuntu/.chezmoitemplates/init.sh
+++ ubuntu/.chezmoitemplates/init.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
 function exists() {
-  command -v "${@}" > "/dev/null" 2>&1
-}
-
-function info() {
-  if exists rich; then
-    rich --print --style 'bold bright_blue' "${*}"
-  else
-    echo -e -n "\x1b[1;94m"
-    echo -n "${*}"
-    echo -e "\x1b[0m"
-  fi
+  command -v "${@}" > /dev/null 2>&1
 }
 
 function run() {
-  info "+ ${*}"
+  if exists gum; then
+    local prefix="$(gum style --background=14 --padding="0 1" RUN)"
+    local message="$(gum style --foreground=14 "${*}")"
+    gum join --horizontal "${prefix}" " " "${message}"
+  fi
   "${@}"
 }
