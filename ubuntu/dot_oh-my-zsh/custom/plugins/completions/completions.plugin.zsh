fb1af95b95cb047823296880b54c3161544679a0
Qin Li <liblaf@outlook.com>
2023-02-27 14:51:52 +0800

    fix: update aliases

--- ubuntu/dot_oh-my-zsh/custom/plugins/completions/completions.plugin.zsh
+++ ubuntu/dot_oh-my-zsh/custom/plugins/completions/completions.plugin.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 
 # git-extras
-if [[ -r "/home/linuxbrew/.linuxbrew/opt/git-extras/share/git-extras/git-extras-completion.zsh" ]]; then
-  source "/home/linuxbrew/.linuxbrew/opt/git-extras/share/git-extras/git-extras-completion.zsh"
+if [[ -r /home/linuxbrew/.linuxbrew/opt/git-extras/share/git-extras/git-extras-completion.zsh ]]; then
+  source /home/linuxbrew/.linuxbrew/opt/git-extras/share/git-extras/git-extras-completion.zsh
 fi
