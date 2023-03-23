ddf82662a4610a425afc41091a539446a6cf7027
liblaf <i@liblaf.me>
2023-03-23 18:10:26 +0800

    fix: config clash

--- archlinux/.chezmoitemplates/init.sh
+++ archlinux/.chezmoitemplates/init.sh
@@ -4,9 +4,24 @@ function exists() {
 
 function run() {
   if exists gum; then
-    local prefix="$(gum style --background=14 --padding="0 1" RUN)"
+    local prefix="$(gum style --background=14 --foreground=0 --padding="0 1" RUN)"
     local message="$(gum style --foreground=14 "${*}")"
     gum join --horizontal "${prefix}" " " "${message}"
   fi
   "${@}"
+  local ret=$?
+  if exists gum; then
+    if ((ret != 0)); then
+      local prefix="$(gum style --background=9 --foreground=7 --padding="0 1" "FAIL ${ret}")"
+      local message="$(gum style --foreground=9 "${*}")"
+      gum join --horizontal "${prefix}" " " "${message}"
+    fi
+  fi
+}
+
+function run-safe() {
+  (
+    set +o errexit
+    run "${@}"
+  )
 }
