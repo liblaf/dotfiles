f14c41ec9a19212e8871ffa94a0fbe1d5b77da2d
liblaf <i@liblaf.me>
2023-08-04 07:55:28 +0800

    fix(legion-y9000p): config rclone

--- scripts/setup-rclone.sh
+++ scripts/setup-rclone.sh
@@ -8,30 +8,30 @@ function check() {
     dasel --read=json "${1}.token" &> /dev/null
 }
 
-if check onedrive-business; then
+if check business; then
   echo -e "\x1b[1;92mOnedrive Business Already Configured\x1b[0m"
 else
   if [[ -z ${BW_SESSION:-""} ]]; then
     echo -e "\x1b[1;91mBitwarden Locked\x1b[0m"
     exit 1
   else
-    client_id="$(bw get username Rclone)"
-    client_secret="$(bw get password Rclone)"
+    client_id="$(bw get username "Rclone Business")"
+    client_secret="$(bw get password "Rclone Business")"
     notify-send --transient "Config Onedrive Business ..."
-    rclone config create onedrive-business onedrive client_id="${client_id}" client_secret="${client_secret}"
+    rclone config create business onedrive client_id="${client_id}" client_secret="${client_secret}"
   fi
 fi
 
-if check onedrive-personal; then
+if check personal; then
   echo -e "\x1b[1;92mOnedrive Personal Already Configured\x1b[0m"
 else
   if [[ -z ${BW_SESSION:-""} ]]; then
     echo -e "\x1b[1;91mBitwarden Locked\x1b[0m"
     exit 1
   else
-    client_id="$(bw get username Rclone)"
-    client_secret="$(bw get password Rclone)"
+    client_id="$(bw get username "Rclone Personal")"
+    client_secret="$(bw get password "Rclone Personal")"
     notify-send --transient "Config Onedrive Personal ..."
-    rclone config create onedrive-personal onedrive client_id="${client_id}" client_secret="${client_secret}"
+    rclone config create personal onedrive client_id="${client_id}" client_secret="${client_secret}"
   fi
 fi
