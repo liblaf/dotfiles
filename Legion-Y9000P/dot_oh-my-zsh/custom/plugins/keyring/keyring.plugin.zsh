f22e90f01c79acafe711862b1d604d35ff994e83
liblaf <i@liblaf.me>
2023-08-21 18:33:19 +0800

    fix: fix keyring

--- Legion-Y9000P/dot_oh-my-zsh/custom/plugins/keyring/keyring.plugin.zsh
+++ Legion-Y9000P/dot_oh-my-zsh/custom/plugins/keyring/keyring.plugin.zsh
@@ -2,6 +2,8 @@
 
 # https://stackoverflow.com/a/76049791
 function keyring-unlock() {
-  read -r -s -p "Password: " password
+  echo -n "[keyring] password for $(whoami): "
+  read -r -s password
+  echo
   eval $(echo -n ${password} | gnome-keyring-daemon --replace --unlock)
 }
