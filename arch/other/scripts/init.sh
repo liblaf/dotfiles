d5390316159b5e14e2b0194a6de47e890a60d814
liblaf <i@liblaf.me>
2023-10-08 16:10:46 +0800

    fix(arch): config jellyfin

--- arch/other/scripts/init.sh
+++ arch/other/scripts/init.sh
@@ -3,13 +3,17 @@ function has() {
 }
 
 function run() {
-  (
-    echo -e "\x1b[1;92;100m>>> $*\x1b[0m"
-    "$@"
-  )
-  local ret=$?
-  if ((ret != 0)); then
-    echo -e "\x1b[1;91m\uf00d $ret >>>" "$*" "\x1b[0m" #  nf-fa-close
+  if command run true &> /dev/null; then
+    command run "$@"
+  else
+    (
+      echo -e "\x1b[1;92;100m>>> $*\x1b[0m"
+      "$@"
+    )
+    local ret=$?
+    if ((ret != 0)); then
+      echo -e "\x1b[1;91m\uf00d $ret >>>" "$*" "\x1b[0m" #  nf-fa-close
+    fi
   fi
 }
 
