69d6b7e236d88a1d92ea69802e00b414598f32f4
liblaf <i@liblaf.me>
2023-10-25 14:02:46 +0800

    refactor: improve fish shell configuration and package management setup

---
+++ arch/dot_config/fish/conf.d/mamba.fish
@@ -0,0 +1,6 @@
+# >>> mamba initialize >>>
+# !! Contents within this block are managed by 'mamba init' !!
+set --global --export MAMBA_EXE /usr/bin/micromamba
+set --global --export MAMBA_ROOT_PREFIX $HOME/micromamba
+$MAMBA_EXE shell hook --shell=fish --root-prefix=$MAMBA_ROOT_PREFIX | source
+# <<< mamba initialize <<<
