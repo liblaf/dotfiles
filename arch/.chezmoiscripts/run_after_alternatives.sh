9db90677f9803ecccf3267cc91552d7f52484a3a
liblaf <30631553+liblaf@users.noreply.github.com>
2023-12-20 22:51:59 +0800

    feat: add Nautilus as the default application for opening directories
    Add Nautilus as the default application for opening directories in the mimeapps.list file.
    This change is made to ensure a consistent and user-friendly experience for opening directories.

--- arch/.chezmoiscripts/run_after_alternatives.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o errexit
-set -o nounset
-set -o pipefail
-
-if [[ -f /usr/bin/go-task ]]; then
-  sudo ln --force --symbolic --no-target-directory --verbose \
-    /usr/bin/go-task /usr/local/bin/task
-fi
