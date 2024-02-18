caad625a174f2e3c0ca674f198298e970cc96361
liblaf <30631553+liblaf@users.noreply.github.com>
2024-02-19 02:24:26 +0800

    fix(arch/dot_config/fish): fix condition for checking symbolic link equivalence
    Update the condition in the script to correctly check if the configuration file is not a symbolic link to another file. This ensures that the correct configuration file is being used for environment variable settings.

--- arch/dot_config/fish/conf.d/environment.fish
+++ arch/dot_config/fish/conf.d/environment.fish
@@ -1,6 +1,6 @@
 fish_add_path --global "$HOME/.local/bin"
 for config in $HOME/.config/environment.d/*.conf
-    if test "$config" -ef "$HOME/.config/environment.d/path.conf"
+    if test ! "$config" -ef "$HOME/.config/environment.d/path.conf"
         export (
             envsubst <$config |
                 sed --expression="s/\s*#.*//" --expression='/^\s*$/d' --regexp-extended
