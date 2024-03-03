1fc5ef9ee452c69508328ea1c19bb8bcd9dd09a4
liblaf <30631553+liblaf@users.noreply.github.com>
2024-03-03 16:43:10 +0800

    fix: pass command line arguments to cspell-init script
    Pass command line arguments provided to the cspell-init script in the `cspell-init.fish` function. Previously, the script was not receiving any command line arguments, which could limit its functionality. By passing the arguments using `$argv`, the script can now utilize additional input parameters as needed.
    
    The changes in this commit ensure that the `cspell-init.fish` function can make use of command line arguments when executing the `cspell-init.py` script.

--- arch/.external/fish/misc/functions/cspell-init.fish
+++ arch/.external/fish/misc/functions/cspell-init.fish
@@ -2,7 +2,7 @@ function cspell-init
     set --function git_root (git rev-parse --show-toplevel)
     pushd $git_root
     command rm --force --verbose {.,}c{s,S}pell{,.config}{.json,.js,.cjs,.yaml,.yml}
-    python "$HOME/github/liblaf/repo/scripts/cspell-init.py" >".cspell.json"
+    python "$HOME/github/liblaf/repo/scripts/cspell-init.py" $argv >".cspell.json"
     prettier --write ".cspell.json"
     popd
 end
--- arch/dot_config/wezterm/wezterm.lua
+++ arch/dot_config/wezterm/wezterm.lua
@@ -2,6 +2,12 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font 'FiraCode Nerd Font'
+config.font = wezterm.font_with_fallback {
+  {
+    family = 'MonaspiceNe Nerd Font',
+    harfbuzz_features = { 'ss01', 'ss02', 'ss03', 'ss06', 'ss07', 'ss08', 'calt', 'dlig' },
+  },
+  'FiraCode Nerd Font'
+}
 
 return config
