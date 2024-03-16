618bfdba1195222f36d4ee785efaac78cd602e9a
liblaf <30631553+liblaf@users.noreply.github.com>
2024-03-16 19:35:08 +0800

    feat: update package mirror URLs and script names
    Updated package mirror URLs to include trailing slashes for consistency. Renamed setup script to better reflect its purpose. Added new script for package management tasks. Removed a script related to a deprecated tool. Introduced systemd script for system management tasks. Removed a command from post-install script.

--- arch/.chezmoiscripts/run_before_10-pueue.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -o errexit
-set -o nounset
-set -o pipefail
-
-systemctl --user enable --now pueued.service
-pueue reset
-pueue group add "chezmoi" --parallel 8 || true
--- post-install.sh
+++ post-install.sh
@@ -7,4 +7,3 @@ workspace=$(realpath "$(dirname "$0")")
 source "$workspace/scripts/setup/bitwarden.sh"
 bash "$workspace/scripts/setup/github.sh"
 bash "$workspace/scripts/setup/rclone.sh"
-pueue status
