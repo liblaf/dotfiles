ba7ff50521cf2a010be13fb4046d76ce63f335d0
liblaf <30631553+liblaf@users.noreply.github.com>
2024-04-24 17:17:18 +0800

    fix: add missing flag to 'ls' alias for Git integration
    The commit fixes a bug where the '--git' flag was missing in the 'ls' alias, ensuring proper Git integration.

--- arch/dot_config/fish/conf.d/eza.fish
+++ arch/dot_config/fish/conf.d/eza.fish
@@ -1,5 +1,5 @@
 if status is-interactive
     if type --query eza >/dev/null
-        alias ls 'eza --icons --time-style long-iso'
+        alias ls 'eza --icons --time-style long-iso --git'
     end
 end
