ab7468ce72d0ee223f75bb6fda3a2126050a8be8
liblaf <30631553+liblaf@users.noreply.github.com>
2024-05-04 14:17:44 +0800

    fix: update font configuration to include Cascadia Code Nerd Font
    Switched the font configuration to include Cascadia Code Nerd Font alongside other fonts for better compatibility and aesthetics.

--- arch/dot_config/wezterm/wezterm.lua
+++ arch/dot_config/wezterm/wezterm.lua
@@ -2,12 +2,12 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+-- TODO: switch to Cascadia Code
+-- https://github.com/microsoft/cascadia-code/issues/730
 config.font = wezterm.font_with_fallback {
-  {
-    family = 'MonaspiceNe Nerd Font',
-    harfbuzz_features = { 'ss01', 'ss02', 'ss03', 'ss06', 'ss07', 'ss08', 'calt', 'dlig' },
-  },
-  'FiraCode Nerd Font'
+  'CaskaydiaCove Nerd Font',
+  'MonaspiceNe Nerd Font',
+  'FiraCode Nerd Font',
 }
 
 return config
