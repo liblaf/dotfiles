fc8591db0f2a292df7f5b00cc5b7a07aa57560f8
liblaf <30631553+liblaf@users.noreply.github.com>
2024-05-16 15:46:01 +0800

    fix: resolve issue with missing checksum file
    Ensure the checksum file is generated before syncing and checking files with rclone.

---
+++ arch/dot_local/bin/executable_rclone-archive.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+source=$(realpath -- "$1")
+pushd -- "$source"
+year=$(date +%Y)
+date=$(date +%Y-%m-%d)
+name=$(basename -- "$source")
+target=business:archive/$year/$date-$name
+command rm --force -- "$source/b2sums.txt"
+find "$source" -type f -printf "%P\n" |
+  sort |
+  xargs --delimiter="\n" --max-args="1" --no-run-if-empty -- b2sum > "$source/b2sums.txt"
+task_id=$(pueue add --working-directory "$source" --print-task-id -- rclone sync --progress "$source" "$target")
+pueue add --after "$task_id" -- rclone check "$source" "$target"
+popd
