48111c348f32c24ebdf00e088e0cfdff27ad33ad
liblaf <30631553+liblaf@users.noreply.github.com>
2024-05-25 22:44:16 +0800

    fix: exclude '.git' directory from rsync command
    Prevent syncing the '.git' directory when pushing or pulling changes to/from a remote repository, improving efficiency and avoiding unnecessary data transfer.

--- arch/dot_local/chezmoi/fish/rsync/functions/rsync-git-push.fish
+++ arch/dot_local/chezmoi/fish/rsync/functions/rsync-git-push.fish
@@ -6,7 +6,7 @@ function rsync-git-push --argument-names remote
     set --function git_root (git rev-parse --show-toplevel)
     if test -d "$git_root"
         echo "'$git_root' -> $remote"
-        command rsync --info="progress2" --archive --delete --force --filter=":- .gitignore" --stats --human-readable --progress --itemize-changes "$git_root/" "$remote:$git_root"
+        command rsync --info="progress2" --archive --delete --force --filter=":- .gitignore" --exclude=".git" --stats --human-readable --progress --itemize-changes "$git_root/" "$remote:$git_root"
     else
         return 1
     end
