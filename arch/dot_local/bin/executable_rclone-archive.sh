b8771f4b87a80054a6e2965b20ce6b0f5adaca19
liblaf <30631553+liblaf@users.noreply.github.com>
2024-05-31 21:04:54 +0800

    fix: update rsync exclude pattern to avoid locking files
    Adjust rsync exclude pattern to avoid locking files with *.lock extension when syncing directories.

--- arch/dot_local/bin/executable_rclone-archive.sh
+++ arch/dot_local/bin/executable_rclone-archive.sh
@@ -3,12 +3,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+remote=${remote:-business:archive}
 source=$(realpath -- "$1")
+
 pushd -- "$source"
 year=$(date +%Y)
 date=$(date +%Y-%m-%d)
 name=$(basename -- "$source")
-target=business:archive/$year/$date-$name
+target=$remote/$year/$date-$name
 command rm --force -- "$source/b2sums.txt"
 find "$source" -type f -printf "%P\n" |
   sort |
--- arch/dot_local/chezmoi/fish/rsync/functions/rsync-git-pull.fish
+++ arch/dot_local/chezmoi/fish/rsync/functions/rsync-git-pull.fish
@@ -6,7 +6,7 @@ function rsync-git-pull --argument-names remote
     set --function git_root (git rev-parse --show-toplevel)
     if test -d "$git_root"
         echo "$remote -> '$git_root'"
-        command rsync --info="progress2" --archive --delete --force --filter=":- .gitignore" --exclude=".git" --stats --human-readable --progress --itemize-changes "$remote:$git_root/" "$git_root"
+        command rsync --info="progress2" --archive --delete --force --filter=":- .gitignore" --exclude="*.lock" --stats --human-readable --progress --itemize-changes "$remote:$git_root/" "$git_root"
     else
         return 1
     end
