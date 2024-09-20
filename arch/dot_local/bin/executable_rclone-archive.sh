3082fff830f387be0df0ba1f428554c7302f588a
liblaf <30631553+liblaf@users.noreply.github.com>
2024-09-20 15:22:06 +0800

    fix: correct backup script and configuration for improved reliability
    - Ensure backup scripts correctly handle source directories and mount points
    - Remove redundant or outdated configurations to streamline setup
    - Update external tool configurations for better integration and maintenance

--- arch/dot_local/bin/executable_rclone-archive.sh
+++ arch/dot_local/bin/executable_rclone-archive.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
-set -o errexit
-set -o nounset
-set -o pipefail
+set -o errexit -o nounset -o pipefail
 
-# TODO: change to SeaDrive
-remote=${remote:-"business:/archive"}
-source=$(realpath -- "$1")
+function parser_definition() {
+  setup REST help:usage -- "Usage: $0 [options]... [arguments]..." ''
+  msg -- 'Options:'
+  param FORMAT -f --format init:=".tar.xz"
+  param HASH -h --hash init:="sha256"
+  param REMOTE -r --remote init:="$HOME/SeaDrive/My Libraries/archive"
+  disp :usage --help
+}
 
-pushd -- "$source"
+eval "$(getoptions parser_definition) exit 1"
+SOURCE=$(realpath -- "$1")
+
+pushd -- "$SOURCE"
 year=$(date +%Y)
 date=$(date +%Y-%m-%d)
-name=$(basename -- "$source")
-target=$remote/$year/$date-$name
-command rm --force -- "$source/b2sums.txt"
-find "$source" -type f -printf "%P\n" |
-  sort |
-  xargs --delimiter="\n" --max-args="1" --no-run-if-empty -- b2sum > "$source/b2sums.txt"
-task_id=$(pueue add --working-directory "$source" --print-task-id -- rclone sync --progress "$source" "$target")
-pueue add --after "$task_id" -- rclone check "$source" "$target"
+name=$(basename -- "$SOURCE")
+sumfile=$SOURCE/${HASH}sums.txt
+target=$REMOTE/$year/$date-$name
+archive=$target/$name$FORMAT
+rclone hashsum "$HASH" "$SOURCE" --output-file "$sumfile" --exclude "*sums.txt"
+rclone sync --progress "$SOURCE" "$target/$name"
+ouch compress "$SOURCE" "$archive"
+rclone hashsum "$HASH" "$archive" --output-file "$archive.${HASH}"
 popd
