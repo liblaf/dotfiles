243a9410a65b072de88cf27cfacfeb9b57aa31a3
liblaf <30631553+liblaf@users.noreply.github.com>
2024-09-21 22:48:18 +0800

    fix: correct Rust package reference and remove redundant script
    Remove outdated Rustup script and update package list to use the correct Rust package. Additionally, add support for Windows in the build process.

---
+++ ventoy/makefiles/windows.mk
@@ -0,0 +1,13 @@
+DEST_DIR ?= $(error DEST_DIR is not set)
+WIN_DIR  := $(DEST_DIR)/windows
+
+.PHONY: windows
+windows: $(WIN_DIR)/win11x64.iso
+
+$(WIN_DIR)/%.iso: $(WIN_DIR)/Mido.sh
+	@ mkdir --parents --verbose "$(@D)"
+	@ bash "$<" "$*"
+
+$(WIN_DIR)/Mido.sh:
+	@ mkdir --parents --verbose "$(@D)"
+	wget --output-document="$@" "https://raw.githubusercontent.com/ElliotKillick/Mido/main/Mido.sh"
