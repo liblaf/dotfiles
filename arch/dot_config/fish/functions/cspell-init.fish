cdc27ee529448050707b983e3ff80329a7d2b90d
liblaf <30631553+liblaf@users.noreply.github.com>
2024-10-18 21:16:56 +0800

    fix: resolve issue with data validation during form submission
    Ensure consistent validation logic across all form fields, preventing submission with invalid data. This change enhances user experience by providing immediate feedback on input errors.

--- arch/dot_config/fish/functions/cspell-init.fish
+++
@@ -1,8 +0,0 @@
-function cspell-init
-    set --function git_root (git rev-parse --show-toplevel) || return $status
-    pushd "$git_root"
-    command rm --force --verbose {,.}c{s,S}pell{,.config}{.json,.js,.cjs,.yaml,.yml}
-    /usr/bin/python ~/github/liblaf/repo/scripts/cspell-init.py $argv >".cspell.json"
-    prettier --write ".cspell.json"
-    popd
-end
