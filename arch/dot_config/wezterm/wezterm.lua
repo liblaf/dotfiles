763c724578923113129911214d56bc5bb8fd35a1
liblaf <30631553+liblaf@users.noreply.github.com>
2024-10-25 21:10:04 +0800

    chore: update GNOME settings and WezTerm configuration
    - Change monospace font in GNOME settings to MonaspiceNe Nerd Font
    - Update favorite apps in GNOME shell to include WezTerm
    - Enhance WezTerm configuration with additional fallback fonts and startup behavior

--- arch/dot_config/wezterm/wezterm.lua
+++ arch/dot_config/wezterm/wezterm.lua
@@ -1,13 +1,24 @@
 local wezterm = require "wezterm"
-
+local mux = wezterm.mux
 local config = wezterm.config_builder()
 
--- TODO: switch to Cascadia Code
--- https://github.com/microsoft/cascadia-code/issues/730
 config.font = wezterm.font_with_fallback {
   "CaskaydiaCove Nerd Font",
   "MonaspiceNe Nerd Font",
   "FiraCode Nerd Font",
+  "Cascadia Code",
+  "Monaspace Neon",
+  "FiraCode",
+  "JetBrains Mono",
+  "Symbols Nerd Font",
+  "Noto Color Emoji",
+  "Noto Sans",
+  "Noto Sans CJK SC",
 }
+
+wezterm.on('gui-startup', function(cmd)
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
 
 return config
