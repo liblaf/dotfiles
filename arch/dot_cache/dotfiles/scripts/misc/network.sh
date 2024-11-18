896b9064195b5d0159a69ad1aeb37305ae291254
liblaf <30631553+liblaf@users.noreply.github.com>
2024-11-18 22:23:24 +0800

    fix: ensure script does not fail silently on network modification

--- arch/dot_cache/dotfiles/scripts/misc/network.sh
+++ arch/dot_cache/dotfiles/scripts/misc/network.sh
@@ -4,4 +4,4 @@ set -o nounset
 set -o pipefail
 
 # https://thu.services/services/#networkmanager
-nmcli connection modify "Tsinghua-Secure" 802-1x.phase1-auth-flags tls-1-0-enable
+nmcli connection modify "Tsinghua-Secure" 802-1x.phase1-auth-flags tls-1-0-enable || true
