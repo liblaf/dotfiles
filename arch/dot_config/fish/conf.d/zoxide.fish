35845d5389eb95f909014a642459b8b90b9b5150
liblaf <30631553+liblaf@users.noreply.github.com>
2024-11-20 19:28:41 +0800

    chore: configure environment management and directory navigation tools
    - Set up direnv for strict environment loading and whitelisting specific directories.
    - Configure zoxide to exclude certain directories from directory navigation.
    - Update fish shell configuration to initialize zoxide if available.

--- arch/dot_config/fish/conf.d/zoxide.fish
+++ arch/dot_config/fish/conf.d/zoxide.fish
@@ -1,7 +1,6 @@
 if status is-interactive
     # https://github.com/ajeetdsouza/zoxide/blob/main/init.fish
-    if command -sq zoxide
-        set --global --export _ZO_EXCLUDE_DIRS "$HOME:$HOME/OneDrive/*:$HOME/SeaDrive/*"
+    if type --query zoxide
         zoxide init fish | source
     else
         echo 'zoxide: command not found, please install it from https://github.com/ajeetdsouza/zoxide'
