d1c8f1b602d89550fb74ae3b9da1c3541cc297e9
liblaf <30631553+liblaf@users.noreply.github.com>
2024-11-23 12:47:40 +0800

    feat: add proxy configuration script and integrate into build process
    Introduce a new proxy-on.sh script to configure system-wide proxy settings.
    Integrate the script into the build process by adding an assets target in the Makefile.

--- ventoy/makefiles/windows.mk
+++ ventoy/makefiles/windows.mk
@@ -6,8 +6,9 @@ windows: $(WIN_DIR)/win11x64.iso
 
 $(WIN_DIR)/%.iso: $(WIN_DIR)/Mido.sh
 	@ mkdir --parents --verbose "$(@D)"
-	@ bash "$<" "$*"
+	@- bash "$<" "$*"
 
+.PHONY: $(WIN_DIR)/Mido.sh
 $(WIN_DIR)/Mido.sh:
 	@ mkdir --parents --verbose "$(@D)"
 	wget --output-document="$@" "https://raw.githubusercontent.com/ElliotKillick/Mido/main/Mido.sh"
