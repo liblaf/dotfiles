4532016f6c217a304efa381d93a4f4bcc8c8e6bd
liblaf <30631553+liblaf@users.noreply.github.com>
2024-12-26 10:52:40 +0800

    refactor: improve external directory handling and NVIDIA detection logic
    - Introduces a template variable for external directory path to enhance flexibility
    - Restructures NVIDIA detection script for better readability and maintainability
    - Updates external file references to use the new template variable
    - Removes default value for NVIDIA_DRIVER environment variable

--- scripts/detect/nvidia.sh
+++ scripts/detect/nvidia.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 # https://wiki.archlinux.org/title/NVIDIA#Installation
-device=$(lspci -d ::03xx | grep --ignore-case nvidia)
-if [[ -z $device ]]; then
-  exit 0
-fi
-export NVIDIA=true
-case "$device" in
-  *GP102*) export NVIDIA_DRIVER=proprietary ;;
-  *) export NVIDIA_DRIVER=open ;;
-esac
-if echo "$device" | grep --ignore-case mobile > /dev/null; then
-  export NVIDIA_MOBILE=true
+device=$(
+  lspci -d ::03xx | grep --ignore-case nvidia
+  true
+)
+if [[ -n $device ]]; then
+  export NVIDIA=true
+  case "$device" in
+    *GP102*) export NVIDIA_DRIVER=proprietary ;;
+    *) export NVIDIA_DRIVER=open ;;
+  esac
+  if echo "$device" | grep --ignore-case mobile > /dev/null; then
+    export NVIDIA_MOBILE=true
+  fi
 fi
