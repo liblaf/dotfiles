f224a2e2255891cf5d7c3a6ddc6dddb47f0b7534
liblaf <30631553+liblaf@users.noreply.github.com>
2025-02-03 17:55:52 +0800

    refactor: improve chezmoi config handling in install.sh
    - Uses a variable for chezmoi config path
    - Reorders logic to handle chezmoi initialization more clearly

--- install.sh
+++ install.sh
@@ -9,10 +9,11 @@ source "$workspace/scripts/detect/intel.sh"
 source "$workspace/scripts/detect/nvidia.sh"
 source "$workspace/scripts/setup/bitwarden.sh"
 
-rm --force --verbose ~/.config/chezmoi/chezmoi.*
-if [[ -d ~/.local/share/chezmoi ]]; then
-  chezmoi init liblaf
+chezmoi_config="$HOME/.config/chezmoi/"
+rm --force --verbose "$chezmoi_config"/chezmoi.*
+if [[ -d ~/.local/share/chezmoi/ ]]; then
+  chezmoi execute-template < "$workspace/.chezmoi.toml.tmpl" > "$chezmoi_config/chezmoi.toml"
 else
-  chezmoi execute-template < "$workspace/.chezmoi.toml.tmpl" > ~/.config/chezmoi/chezmoi.toml
+  chezmoi init liblaf
 fi
 chezmoi apply "$@"
