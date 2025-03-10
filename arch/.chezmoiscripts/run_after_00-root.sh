1d050a0975820fdf9d2af7cd20dc6859637fc588
liblaf <30631553+liblaf@users.noreply.github.com>
2025-03-10 14:11:44 +0800

    chore: simplify root file copy command and rename Caddyfile template
    - removes redundant `--backup` flag from `cp` command
    - renames `private_Caddyfile.tmpl` to `Caddyfile.tmpl` for consistency

--- arch/.chezmoiscripts/run_after_00-root.sh
+++ arch/.chezmoiscripts/run_after_00-root.sh
@@ -3,4 +3,4 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-sudo cp --archive --backup --force --no-preserve="ownership" --verbose "$HOME/.cache/dotfiles/root/"* "/"
+sudo cp --archive --force --no-preserve="ownership" --verbose "$HOME/.cache/dotfiles/root/"* "/"
