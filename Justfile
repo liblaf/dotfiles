931989cf2c80457a9da28112ab4be9010245a3d7
liblaf <30631553+liblaf@users.noreply.github.com>
2025-03-20 17:23:29 +0800

    feat(starship): add starship configuration generator and update configs
    - Introduces a new generator script for Starship configuration
    - Updates Starship configs with new symbols and formats
    - Adds `rust-src` to extra packages for Rust development
    - Removes `STARSHIP_SHELL` export in Fish config

---
+++ Justfile
@@ -0,0 +1,6 @@
+default: gen
+
+gen: gen-starship
+
+gen-starship:
+  python "gen/starship/main.py"
--- arch/dot_config/fish/conf.d/starship.fish
+++ arch/dot_config/fish/conf.d/starship.fish
@@ -1,4 +1,3 @@
 if status is-interactive
     starship init fish | source
-    set --global --export STARSHIP_SHELL sh
 end
