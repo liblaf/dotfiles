da3f0b673d6f6667cb6b024c77d5fec08f4d8bed
liblaf <30631553+liblaf@users.noreply.github.com>
2025-04-27 11:27:13 +0800

    refactor: reorganize service scripts and update configurations
    - moves service scripts to dedicated services directory
    - comments out tldr package and caddy envfile ownership
    - reorders grub script execution sequence
    - consolidates sudoers env_keep syntax

--- cachyos/.chezmoiscripts/run_after_00-root.sh
+++ cachyos/.chezmoiscripts/run_after_00-root.sh
@@ -5,4 +5,5 @@ set -o pipefail
 
 sudo cp --archive --force --no-preserve="ownership" --verbose "$HOME/.cache/dotfiles/root/"* "/"
 sudo chmod "u=rwx,g=rx,o=" /var/lib/caddy
-sudo chown "caddy" /var/lib/caddy/envfile
+# sudo chown "caddy" /var/lib/caddy/envfile
+# TODO: fix
--- cachyos/dot_cache/exact_dotfiles/exact_scripts/exact_services/docker.sh
+++ cachyos/dot_cache/exact_dotfiles/exact_scripts/exact_services/docker.sh
@@ -3,6 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+sudo systemctl enable --now docker.service
+
 cd ~/services
 sudo docker compose pull
 sudo docker compose up --detach || true
