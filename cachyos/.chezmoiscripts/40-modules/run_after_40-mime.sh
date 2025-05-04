122c731fd9cf627b54e907b327ff83a13b8ce9ea
liblaf <30631553+liblaf@users.noreply.github.com>
2025-05-04 17:55:24 +0800

    feat(restic): add restic-browser-bin package and configure resticprofile
    - Adds restic-browser-bin to pacman packages
    - Configures restic port and schedule in resticprofile
    - Reorganizes restic-related scripts and service configuration
    - Updates password file creation to use install command

--- cachyos/.chezmoiscripts/40-modules/run_after_40-mime.sh
+++ cachyos/.chezmoiscripts/40-modules/run_after_40-mime.sh
@@ -3,5 +3,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# ref: https://wiki.archlinux.org/title/XDG_MIME_Applications
+# ref: <https://wiki.archlinux.org/title/XDG_MIME_Applications>
 update-mime-database "$HOME/.local/share/mime"
--- cachyos/.chezmoiscripts/90-misc/run_after_90-tsinghua-secure.sh
+++ cachyos/.chezmoiscripts/90-misc/run_after_90-tsinghua-secure.sh
@@ -3,5 +3,5 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# https://thu.services/services/#networkmanager
+# ref: <https://thu.services/services/#networkmanager>
 nmcli connection modify "Tsinghua-Secure" 802-1x.phase1-auth-flags tls-1-0-enable || true
