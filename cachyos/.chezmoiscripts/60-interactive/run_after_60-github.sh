8934006bcbbb3c5d0674730acd41cf41296a12bb
liblaf <30631553+liblaf@users.noreply.github.com>
2025-05-05 16:43:10 +0800

    fix: temporarily disable interactive GitHub auth setup
    - Adds TODO comment to re-enable later
    - Exits script early to skip virtual machine detection

--- cachyos/.chezmoiscripts/60-interactive/run_after_60-github.sh
+++ cachyos/.chezmoiscripts/60-interactive/run_after_60-github.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+# shellcheck disable=SC2317
 set -o errexit
 set -o nounset
 set -o pipefail
 
-if systemd-detect-virt; then
-  exit
-fi
+# TODO: re-enable this when I figure out a better way for interactive setup
+exit
+
+if systemd-detect-virt; then exit; fi
 
 if ! gh auth status; then
   gh auth login --git-protocol https --scopes delete_repo --web
