97aa5c3dbc39fd3ecb69206d8bf01855c02f5555
liblaf <30631553+liblaf@users.noreply.github.com>
2026-03-31 13:05:55 +0800

    docs: update pot workaround reference links

--- modules/apps/pot/dot_local/share/applications/run_after_pot.desktop.sh
+++ modules/apps/pot/dot_local/share/applications/run_after_pot.desktop.sh
@@ -4,6 +4,7 @@ set -o nounset
 set -o pipefail
 
 # ? QUESTION: I don't know why, but this is needed to make screenshot work.
+# ref: <https://github.com/flameshot-org/flameshot/issues/3769#issuecomment-2475921012>
 expression='.["Desktop Entry"].Exec = "sh -c \"pot < /dev/null > /dev/null\""'
 yq eval "$expression" '/usr/share/applications/pot.desktop' \
   --input-format 'ini' --output-format 'ini' > 'pot.desktop'
