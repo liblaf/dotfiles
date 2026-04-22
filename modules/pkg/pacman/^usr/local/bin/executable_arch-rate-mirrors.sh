47ce7af1aa8c71cb3f1e73a108b8f88f94cae26d
liblaf <30631553+liblaf@users.noreply.github.com>
2026-04-22 21:30:53 +0800

    fix(pacman): delay arch-rate-mirrors after boot

--- modules/pkg/pacman/^usr/local/bin/executable_arch-rate-mirrors.sh
+++ modules/pkg/pacman/^usr/local/bin/executable_arch-rate-mirrors.sh
@@ -8,6 +8,10 @@ function main() {
   local -r RATE_MIRRORS_SAVE="$(mktemp)"
   local -r RATE_MIRRORS_ALLOW_ROOT=true
   export RATE_MIRRORS_SAVE RATE_MIRRORS_ALLOW_ROOT
+  # TODO: rate-mirrors downloads the upstream mirror list from GitHub, which may
+  # be unreachable behind the GFW. Setting `$https_proxy` forces all requests
+  # (including mirror probes) through the proxy, but we only need GitHub traffic
+  # proxied. We need a better solution.
   rate-mirrors "$@"
   sudo install --backup='simple' --mode='u=rw,go=r' --suffix='-backup' --verbose \
     "$RATE_MIRRORS_SAVE" "/etc/pacman.d/$repo-mirrorlist"
