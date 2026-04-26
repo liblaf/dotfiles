6d8551e1e7e286acc6487308491ab007adfb87f4
liblaf <30631553+liblaf@users.noreply.github.com>
2026-04-26 12:54:41 +0800

    chore(dotfiles): template tool configs with home paths
    Move Git and Codex configs to chezmoi templates so home-relative paths render explicitly. Add a repository-level Tombi config and ignore a noisy journal notification error.

--- modules/dev/git/.scripts/run_after_50-prek.sh
+++ modules/dev/git/.scripts/run_after_50-prek.sh
@@ -4,5 +4,4 @@ set -o nounset
 set -o pipefail
 
 template_dir="$(git config init.templateDir)"
-template_dir="${template_dir/#'~'/$HOME}"
 prek util init-template-dir --hook-type pre-commit "$template_dir"
